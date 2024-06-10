8ba1bdb96f3e4422f30b69335cb290a4e30a1887
Adrian <adrian@adriantroop.com>
2024-06-10 11:49:55 +0200

    Adapted test and a blank token we can deploy and test through JS

---
+++ test/Token.js
@@ -0,0 +1,34 @@
+//Testing Contracts
+//import necessary libraries
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), "ether")
+}
+
+describe("Token", ()=>{
+    let token
+    beforeEach(async () => {
+        const Token = await ethers.getContractFactory("Token");
+        token = await Token.deploy("Troopy Coin", "TROC", "1000000");
+    })
+    //Test goes inside here.
+    //What to check for
+    it("has a name", async ()=>{
+        expect(await token.name()).to.equal("Troopy Coin");
+    })
+
+    it("has a symbol", async ()=>{
+        expect(await token.symbol()).to.equal("TROC");
+    })
+
+    it("has correct decimals", async ()=>{
+        expect(await token.decimals()).to.equal("18");
+    })
+
+    it("has correct market cap", async ()=>{
+        const value = tokens("1000000");
+        expect(await token.totalSupply()).to.equal(value);
+    })
+});
